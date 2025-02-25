96f6f0205b2b822d2565daec9dd7abcb4ab08eda
bq就是bq <95249980+necessaryBQ@users.noreply.github.com>
2025-02-24 19:15:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,9 @@ st.write(
 #🥋 Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App. 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 name_on_order = st.text_input("Name on Smoothie:")
